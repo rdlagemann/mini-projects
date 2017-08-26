68a7382480b838f8b1b1d55b861c8290feaacafa
Rafael Lagemann <rdlagemann@gmail.com>
2017-08-26 18:19:15 -0300

    small code refactoring

--- simon-game/js/control.js
+++ simon-game/js/control.js
@@ -1,66 +1,57 @@
-var MAX_TURNS = 20,
-	currentSeq = [],
-	colors,
-	startButton;
+(function () {
+	var MAX_TURNS = 20,
+		currentSeq = [], // current game sequence
+		colors = Array.from(document.querySelectorAll('.simon')),
+		startButton = document.getElementById('startButton');
 
-$(document).ready(function(){
+	startButton.onclick = function(){
+		let last = false,
+			i = 0,
+			intervalID;
 
-	colors = Array.from(document.querySelectorAll('.simon'));
-	console.log(colors);
+		this.disabled = true;		
 
-	startButton = $("#startButton");
-
-	startButton.click(function(e){
-		e.preventDefault();
-		e.stopPropagation();
-		
-		$(this).prop("disabled", true);
-		
-		currentSeq.push(Math.floor(Math.random() * 4)); //between 0 and 3
-
-		let last,
-			intervalID,
-			i = 0;
+		// adds new step in final sequence
+		currentSeq.push(Math.floor(Math.random() * 4));		
 
 		intervalID = setInterval(function(){
-			
-			if(last !== undefined){
-				last.classList.remove("on");
-				last.classList.add("off");
-			}
-
+			// turn light off
+			lightSwitch(last)			
 
 			last = colors[currentSeq[i]];
-			setTimeout(function(){
-				if(last !== undefined){
-					last.classList.remove("off");		
-					last.classList.add("on");
-				}
-				i++;
-				
-			},200);
 
-			if(i >= currentSeq.length-1){
-				setTimeout(function(){
-					if(last !== undefined){
-						last.classList.remove("on");
-						last.classList.add("off");	
-					}
-					last = undefined;	
+			// turn light on
+			setTimeout(function(){				
+				lightSwitch(last);
+				i++;				
+			}, 200);
+
+
+			// ends
+			if(i >= currentSeq.length - 1){	
+				// turn off last light, reset values, stop interval
+				setTimeout(function(){					
+					lightSwitch(last);
+					last = false;	
 					i = 0;
-					startButton.prop("disabled", false);
+					startButton.disabled = false;
 					clearInterval(intervalID);
 
 				}, 900);
-				
 
 			}
+
 		}, 1000);
 
-	});
+	};
 
+})()
 
+function lightSwitch (light) {
+	if(light){
+		light.classList.toggle("off");		
+		light.classList.toggle("on");
+	}	
+}
 
-
-});
 
