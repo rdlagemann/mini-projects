0e8f5adb74ce6e3b5a7cc6ed3bb860398a44d0c3
Rafael Lagemann <rdlagemann@gmail.com>
2017-08-28 15:29:34 -0300

    wip

--- pomodoro-clock/js/control.js
+++ pomodoro-clock/js/control.js
@@ -1,70 +1,76 @@
 (function () {
-	/* macros */
-	const SECONDS = 60;
-	const DEFAULT_MINUTES = 1;
+  /* macros */
+  const SECONDS = 60;
+  var pomodoro_time = 1,
+      break_time = 1,
+      long_break = 1;
 
+  /* clock object */
+  var clock = {
+    minutes: document.getElementById('minutes'),
+    seconds: document.getElementById('seconds'),
+    running: false,
 
-	/* clock object */
-	var clock = {
-		minutes: document.getElementById('minutes'),
-		seconds: document.getElementById('seconds'),
+    in_seconds: {
+      setted: pomodoro_time * SECONDS,
+      current: pomodoro_time * SECONDS
+    },
+    format: function () {
+      if(seconds.innerText.length === 1) {
+        seconds.innerText = "0" + seconds.innerText;
+      }
 
-		in_seconds: {
-			setted: DEFAULT_MINUTES * SECONDS,
-			current: DEFAULT_MINUTES * SECONDS
-		},
-		format: function () {
-			if(seconds.innerText.length === 1) {
-				seconds.innerText = "0" + seconds.innerText;
-			}
+      if(minutes.innerText.length === 1) {
+        minutes.innerText = "0" + minutes.innerText;
+      }
+    },
+    update: function () {
+      this.minutes.innerText = Math.floor(this.in_seconds.current / 60);
+      this.seconds.innerText = (this.in_seconds.current % 60).toString();
+      this.format();  
+      
+    },
+    reset: function () {
+      this.in_seconds.current = this.in_seconds.setted;
+      this.running = false;
+    }
 
-			if(minutes.innerText.length === 1) {
-				minutes.innerText = "0" + minutes.innerText;
-			}
-		},
-		update: function () {
-			this.minutes.innerText = Math.floor(this.in_seconds.current / 60);
-			this.seconds.innerText = (this.in_seconds.current % 60).toString();
-			this.format();	
-			
-		},
-		reset: function () {
-			this.in_seconds.current = this.in_seconds.setted;
-		}
+  };
 
-	};
+  /* preset */
+  clock.update();
 
-	/* preset */
-	clock.update();
+  /* set buttons */
+  var pomodoroButton = document.getElementById('pomodoroButton');
+  var intervalID = false;
 
-	/* set buttons */
-	var startButton = document.getElementById('startButton');
-	
-	startButton.onclick = function () {
-		console.log('start clicked');
+  pomodoroButton.onclick = function () {
+    if(intervalID){
+      clearInterval(intervalID);
+    }
 
-		this.disabled = true;
-		var that = this;
-		
-		var intervalID = setInterval(function () {
-			clock.in_seconds.current -= 1;
-			clock.update();
+    clock.reset();
+    //this.disabled = true;   
+    
+    intervalID = setInterval(function () {
+      clock.in_seconds.current -= 1;
+      clock.update();
 
-			// stop chronometer
-			if (clock.in_seconds.current <= 0) {
-				console.log('finished clock');
-				clearInterval(intervalID);
-				that.disabled = false;
+      // stop chronometer
+      if (clock.in_seconds.current <= 0) {
+        console.log('finished clock');
+        clearInterval(intervalID);
+        pomodoroButton.disabled = false;
 
-			}
+      }
+      
+      // TODO: debug in 100ms, change to 1000ms
+    }, 100);
 
-			// debug
-			console.log(clock.in_seconds.current);
-			
-		}, 100);
-
-	}
-
-
+  }
 
 })()
+
+function runTimer(timer, clock, intervalID) {
+
+}
