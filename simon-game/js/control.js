22914b36964872826da8d3072b7c9b95f235cf24
Rafael Lagemann <rdlagemann@gmail.com>
2017-08-31 15:58:48 -0300

    some code refactoring

--- simon-game/js/control.js
+++ simon-game/js/control.js
@@ -1,57 +1,78 @@
 (function () {
 	var MAX_TURNS = 20,
+		waitingPlayerInput = false,
 		currentSeq = [], // current game sequence
 		colors = Array.from(document.querySelectorAll('.simon')),
 		startButton = document.getElementById('startButton');
 
-	startButton.onclick = function(){
+
+	colors.forEach( function(element) {
+		element.onclick = simonClick;
+
+	});
+
+
+	startButton.onclick = function () { 
+
+		runTurn();
+
+	};
+
+	/*	
+	
+	Function Definitions 
+
+	 */
+
+	function runTurn () {		
 		let last = false,
 			i = 0,
 			intervalID;
 
-		this.disabled = true;		
+		/* adds new step in final sequence */
+		currentSeq.push(Math.floor(Math.random() * 4));	
+		//debug
+		console.log(currentSeq);
 
-		// adds new step in final sequence
-		currentSeq.push(Math.floor(Math.random() * 4));		
+		intervalID = setInterval( function () {
 
-		intervalID = setInterval(function(){
-			// turn light off
-			lightSwitch(last)			
+			playLight(colors[currentSeq[i]]);
 
-			last = colors[currentSeq[i]];
-
-			// turn light on
-			setTimeout(function(){				
-				lightSwitch(last);
-				i++;				
-			}, 200);
-
-
-			// ends
-			if(i >= currentSeq.length - 1){	
-				// turn off last light, reset values, stop interval
-				setTimeout(function(){					
-					lightSwitch(last);
-					last = false;	
-					i = 0;
-					startButton.disabled = false;
-					clearInterval(intervalID);
-
-				}, 900);
-
-			}
+			/* stop condition*/
+			(i >= currentSeq.length) ? clearInterval(intervalID) : i += 1;
 
 		}, 1000);
+			
+	}
 
-	};
+
+	function lightSwitch (light) {	
+		if(light){
+			light.classList.toggle("off");		
+			light.classList.toggle("on");
+		}	
+	}
+
+	function playLight (light) {
+		let intervalID;
+
+		lightSwitch(light);
+
+		intervalID = setTimeout( function () {
+			lightSwitch(light);
+			// TODO: playsound
+			clearInterval(intervalID);
+		}, 500);
+
+
+	}
+
+	function simonClick(){
+		playLight(this);
+	}
+
 
 })()
 
-function lightSwitch (light) {
-	if(light){
-		light.classList.toggle("off");		
-		light.classList.toggle("on");
-	}	
-}
 
 
