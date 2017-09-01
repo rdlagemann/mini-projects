99eceb0ecf00aed12c2f0ffe897dba880f20f284
Rafael Lagemann <rdlagemann@gmail.com>
2017-09-01 00:29:55 -0300

    wip

--- simon-game/js/control.js
+++ simon-game/js/control.js
@@ -1,12 +1,21 @@
 (function () {
 	var MAX_TURNS = 20,
 		waitingPlayerInput = false,
+		inputIterator = 0,
 		currentSeq = [], // current game sequence
+		seqDisplay = document.getElementById('seqDisplay'),
 		colors = Array.from(document.querySelectorAll('.simon')),
-		startButton = document.getElementById('startButton');
+		startButton = document.getElementById('startButton'),
+		sounds = {
+			'greenButton': new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+			'redButton': new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+			'blueButton':  new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+			'yellowButton': new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+		}
 
 
 	colors.forEach( function(element) {
+		
 		element.onclick = simonClick;
 
 	});
@@ -24,13 +33,23 @@
 
 	 */
 
-	function runTurn () {		
+	function runTurn () {	
+		if(currentSeq.length >= MAX_TURNS) {
+			console.log('you win, goodbye');
+			return;
+		}	
+
 		let last = false,
 			i = 0,
 			intervalID;
 
 		/* adds new step in final sequence */
 		currentSeq.push(Math.floor(Math.random() * 4));	
+		// handle one digit number
+		seqDisplay.innerText = currentSeq.length;
+		if (seqDisplay.innerText.length === 1) {
+			seqDisplay.innerText = '0'+ seqDisplay.innerText;
+		}
 		//debug
 		console.log(currentSeq);
 
@@ -38,10 +57,15 @@
 
 			playLight(colors[currentSeq[i]]);
 
-			/* stop condition*/
-			(i >= currentSeq.length) ? clearInterval(intervalID) : i += 1;
+			i += 1;
 
-		}, 1000);
+			/* stop condition*/
+			if(i >= currentSeq.length){
+				clearInterval(intervalID);
+				waitingPlayerInput = true;
+			}			
+
+		}, 800);
 			
 	}
 
@@ -53,22 +77,79 @@
 		}	
 	}
 
+
 	function playLight (light) {
-		let intervalID;
+		let intervalID,
+			button = light;
 
 		lightSwitch(light);
+		console.log(light);
 
 		intervalID = setTimeout( function () {
+
 			lightSwitch(light);
-			// TODO: playsound
+
+			sounds[light.id].play();
+
 			clearInterval(intervalID);
-		}, 500);
+
+		}, 300);
 
 
 	}
 
-	function simonClick(){
+	function checkHit (color, guess) {
+		return (color === guess) ? true : false;
+	}
+
+	function endTurn () {
+		inputIterator = 0;
+		waitingPlayerInput = false;
+		console.log('end turn');
+	}
+
+
+
+	function simonClick () {
+		if (!waitingPlayerInput) {
+			console.log('wait your turn!');
+			return;
+		}
+
+		let lightToGuess = colors[currentSeq[inputIterator]],
+			hit = checkHit(this.id, lightToGuess.id),
+			seqIsOver;
+
+
 		playLight(this);
+		
+		inputIterator += 1;
+
+		seqIsOver = inputIterator >= currentSeq.length;
+
+		if (hit){
+			console.log('hit!');
+			if(seqIsOver){
+				setTimeout(function (){
+					runTurn();
+					return;
+				}, 500);
+				
+			}
+			
+		}
+		else {
+			console.log('error at ' + this.id);
+			endTurn();
+			return;
+		}		
+
+		if (seqIsOver){
+			console.log('current seq ends');
+			endTurn();
+			return;
+		}
+		
 	}
 
 
