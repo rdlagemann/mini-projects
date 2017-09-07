3c2163357bc338b1df148bd6e936a3451bfe058c
Rafael Lagemann <rdlagemann@gmail.com>
2017-09-07 17:15:37 -0300

    some refactoring

--- tic-tac-toe/js/control.js
+++ tic-tac-toe/js/control.js
@@ -1,40 +1,59 @@
 ( function () {
 	console.log('script loaded');
 
-	const gameManager = {
-		table: document.getElementById('tttTable'),
-		matrix: [],
-		updateMatrix: 'oi'
+	const gameBoard = {
+			table: document.getElementById('tttTable'),
+			matrix: [],
+			tableToMatrix: HTMLTableToMatrix,
+			initEmptyMatrix: initEmptyMatrix,
+			init: function () {
+				this.initEmptyMatrix(3, 3);
+				this.tableToMatrix();
+			},
+			checkWin: function () { console.log('check col, check line, check diag')}
+		}	
 
+	const player = {
+		weapon: undefined
 	}
 
-	initEmptyMatrix.call( gameManager.matrix, 3, 3 );
+	const playButton = document.getElementById('playButton');
+	playButton.onclick = function () {
+		console.log('lets play!');
+	}
+	
+	
+	gameBoard.init();
 
-	HTMLTableToMatrix.call( gameManager );
+	console.log(gameBoard.matrix);
+
 
 
 
 	/* HELPER FUNCTIONS */
 
-	function HTMLTableToMatrix () {			
+	function HTMLTableToMatrix () {	
+		if (!this.table || !this.matrix) {
+			return undefined;
+		}	
+
 		let rowLen = this.table.rows.length,
-			colLen = 3,
-			currRow;
+			currentRow,
+			colLen;
 
 		for(let i = 0; i < rowLen; i += 1) {
-			currRow = this.table.rows[i];
-			console.log(currRow.cells[0]);
-			for(let j = 0; j < 3; j += 1) {
-				this.matrix[i][j] = currRow.cells[j].innerText;
+			currentRow = this.table.rows[i];
+			colLen = currentRow.cells.length;
+			for(let j = 0; j < colLen; j += 1) {
+				this.matrix[i][j] = currentRow.cells[j].innerText;
 			}
 		}
 	}
 
 
 	function initEmptyMatrix (rowLen, colLen) {
-		let mat = this;
+		let mat = this.matrix;
 		if(mat.constructor === Array) {
-			console.log('is array');
 			for(var i = 0; i < rowLen; i += 1) {
     			mat[i] = [];    	
 	    		for(var j = 0; j < colLen; j += 1) {
@@ -43,10 +62,9 @@
 			}
 		}
 		else {
-			console.log('type error: not array');
+			throw new TypeError("Cannot build a matrix from 'this'");
 		}
 
-		
 	}
 
 
