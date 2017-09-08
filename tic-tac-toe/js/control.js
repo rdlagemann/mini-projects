4a17811c47307cfb8e07674427218c8ce235ebf2
Rafael Lagemann <rdlagemann@gmail.com>
2017-09-08 15:46:59 -0300

    game funcs, CPU random play
    todo: provide some AI, score, reset button

--- tic-tac-toe/js/control.js
+++ tic-tac-toe/js/control.js
@@ -3,17 +3,14 @@
 
 	/* Weapon 'class' and methods */
 	function Weapon (value, drawClass) {
-		this.value = value;
-		this.drawClass =  drawClass;
+		this.value = value || undefined;
+		this.drawClass =  drawClass || 'noWeaponClass';
 	}
 	Weapon.prototype.setValue  = function ( value ) {
-		value = value.toLowerCase();
-		if (value === 'x' || value === 'o') {
-			this.value = value;
-		}
-		else {
-			value = undefined;
-		}
+		value = (value) ? value.toLowerCase() : undefined;
+		this.value = (value === 'x' || value === 'o') ? value : 'noWeapon';
+		console.log(this.value);
+		this.drawClass = value + 'Class'; // change css class with proper drawing
 	};
 
 	Weapon.prototype.getValue = function () {
@@ -21,20 +18,54 @@
 	};
 
 	Weapon.prototype.draw = function () {
-		this.classList.toggle(this.drawClass);
+		this.classList.add(this.drawClass);
 	}
 
 	/* game objects */
+
+	const gameManager = {
+		humamTurn: true //humam player aways first
+	}
+
 	const gameBoard = {
 		table: document.getElementById('tttTable'),
+		boardCells: [],
+		freeBoardCells: 0,
 		matrix: [],
 		tableToMatrix: HTMLTableToMatrix,
 		initEmptyMatrix: initEmptyMatrix,
 		init: function () {
 			this.initEmptyMatrix(3, 3);
 			this.tableToMatrix();
+			let curr;
+			for (let i = 0; i < this.table.rows.length; i += 1) {
+				curr = this.table.rows[i];
+				for (let j = 0; j < curr.cells.length; j += 1) {
+					curr.cells[j].setValue();
+					this.boardCells.push(curr.cells[j]);
+				}
+			}
+
+			this.freeBoardCells = this.boardCells.length;
 		},
-		checkWin: function () { console.log('check col, check line, check diag')}
+		playWeapon: function ( index, weapon ) {
+			this.boardCells[index].setValue( weapon );
+			this.freeBoardCells -= 1;
+		},
+		drawTable: function () {
+			console.dir(this.table);
+			let curr;
+			this.boardCells.forEach( function (element) {
+				element.draw();
+			})
+		},
+		restart: function () {
+			this.boardCells.forEach( function(element) {
+				element.setValue();
+				element.draw(); 
+			})
+		},
+		checkWin: function () { console.log('check col, check line, check diag'); }
 	}
 
 	const weaponChooser = {
@@ -42,11 +73,62 @@
 		configButtons: configWeaponButtons		
 	}
 
+	const player = {
+		weapon: 'x',
+		score: 0
+	}
+
+	const cpu = {
+		weapon: undefined,
+		score: 0,
+		setWeapon: function () {
+			this.weapon = (player.weapon === 'x') ? 'o' : 'x';
+		},
+		play: function ( gameBoard ) {
+			if (gameBoard.freeBoardCells <= 1) {
+				return;
+			}
+			// AI logic
+			let iRand,
+				isValid = false;
+
+			while( !isValid ) {
+				iRand = Math.floor(Math.random() * 9);
+				if (gameBoard.boardCells[iRand].getValue() === 'noWeapon') {
+					isValid = true;
+				}
+			}
+
+			console.log(gameBoard.boardCells[iRand].getValue());
+
+			gameBoard.playWeapon(iRand, this.weapon );
+			gameBoard.drawTable();
+		}		
+	}
+
+	/* create weapons objects */;
 	const oW = new Weapon('o', 'oClass');
 	const xW = new Weapon('x', 'xClass');
 
+	// extend basic HTML elements
 	extend(xWeapon, xW);
 	extend(oWeapon, oW);
+
+	let cells = Array.from(document.querySelectorAll('td'));
+
+	/* create table functionality */
+	cells.forEach( function (element, index) {
+		extend(element, new Weapon());
+		element.onclick = function () {
+			if (this.value === 'noWeapon' && gameManager.humamTurn) {
+				gameBoard.playWeapon( index, player.weapon );
+				// gameManager.humamTurn = false;
+				gameBoard.checkWin();
+				this.draw();
+				cpu.play( gameBoard );
+			}			
+		}
+	});
 
 	console.dir(xWeapon);
 	
@@ -54,15 +136,14 @@
 	oWeapon.draw();
 
 
-	const player = {
-		weapon: undefined
-	}
-
-
+	/* button will config the game to start */
 	const playButton = document.getElementById('playButton');
 	playButton.onclick = function () {
 		console.log('lets play!');
-		document.getElementById('tttTable').classList.remove('hide');
+		document.getElementById('gameArea').classList.remove('hide');
+		this.parentElement.classList.add('hide');
+		gameBoard.drawTable.call( gameBoard );
+		cpu.setWeapon();
 	}
 
 
@@ -70,9 +151,7 @@
 	gameBoard.init();
 	weaponChooser.configButtons( player );
 
-	//debug
-	console.log(gameBoard.matrix);
-
+	
 
 	/* FUNCTIONS */
 	function extend (target, extension) {
@@ -114,7 +193,7 @@
 			currentRow = this.table.rows[i];
 			colLen = currentRow.cells.length;
 			for(let j = 0; j < colLen; j += 1) {
-				this.matrix[i][j] = currentRow.cells[j].innerText;
+				this.matrix[i][j] = currentRow.cells[j].getValue();
 			}
 		}
 	}
