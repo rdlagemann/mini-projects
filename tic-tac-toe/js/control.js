9e7beb729ce0bf9c8e59643954a915ee90206cc3
Rafael Lagemann <rdlagemann@gmail.com>
2017-09-10 22:25:07 -0300

    cpu random play

--- tic-tac-toe/js/control.js
+++ tic-tac-toe/js/control.js
@@ -1,6 +1,5 @@
-( function ( ) { 
-	"use strict";
-
+( function ( window, undefined ) { 
+	
 	/* Weapon 'class' and methods */
 
 	function Weapon (value) {
@@ -37,8 +36,8 @@
 			player = human;
 
 			/* just two weapon types, so let's hard code */
-			xWeapon = context.getElementById('xWeapon');
-			oWeapon = context.getElementById('oWeapon');
+			xWeapon = context.document.getElementById('xWeapon');
+			oWeapon = context.document.getElementById('oWeapon');
 
 			weapons.push( xWeapon );
 			weapons.push( oWeapon );
@@ -90,7 +89,7 @@
 	const gameBoard = ( function () {
 		/* pvt */
 
-		let table, cells;
+		let table, cells, currentPlayer, humanPlayer, cpuPlayer;
 
 		let checkWin = function () {
 			console.log('cheking win in "cells" ');
@@ -98,23 +97,28 @@
 
 		/* pub */
 
-		let pubInit = function ( context, player ) {
-			table = context.getElementById('tttTable');
-			cells = context.querySelectorAll('td');
-			/* extend table cells */
-			Array.from(context.querySelectorAll('td')).forEach(function (element) {
+		let pubInit = function ( context, player, playerCPU ) {
+			table = context.document.getElementById('tttTable');
+			cells = context.document.querySelectorAll('td');
+			humanPlayer = player;
+			cpuPlayer = playerCPU;
+
+			console.log(humanPlayer);
+			console.log(cpuPlayer);
+
+			/* extend and config table cells */
+			Array.from(cells).forEach(function (element) {
 				extend( element, new Weapon() );
+
 				element.draw();
-				element.onclick = function () {
-					this.setValue( player.weapon.getValue() );
-					this.draw();
-				}
+				
+				element.onclick = playWeapon;
 			});
 
 
 		};
 
-		function pubReset ( context ) {
+		function pubReset () {
 			Array.from(cells).forEach(function (element) {
 				element.setValue();
 				element.draw();
@@ -126,16 +130,48 @@
 			checkWin();
 		};
 
-		function pubDrawBoard ( context ) {
-			Array.from(cells).forEach(function (element) {
-				element.draw();
-			});
+		function cpuPlay ( ) {
+			let len = cells.length,
+				index = 0,
+				isValid = false;
+
+			let prevent = 0;
+	
+			while (!isValid) {
+				prevent += 1;
+
+				if (prevent >= 100) {
+					break;
+				}
+
+				index = Math.floor( Math.random() * 9 );
+
+				if (cells[index].value === 'noWeapon') {
+					isValid = true;
+					cells[index].setValue( cpuPlayer.weapon.value );
+					cells[index].draw();
+				}
+			}
+
+			
+
 		};
+
+		function playWeapon () {
+			this.setValue( humanPlayer.weapon.value );
+			this.draw();
+
+			cpuPlay();
+		}
+
+		function setPlayer ( player ) {
+			currentPlayer = player;
+		}
+
 
 		return {
 			init: pubInit,
 			play: pubPlayWeapon,
-			draw: pubDrawBoard,
 			reset: pubReset
 		}
 
@@ -156,10 +192,10 @@
 		let weapon = new Weapon( wpn ); 
 		let score = 0;
 
-		function F(){};
-		F.prototype = playerProto; 
+		function P(){};
+		P.prototype = playerProto; 
 
-		let player = new F();
+		let player = new P();
 		player.weapon = weapon;
 		player.score = score;
 
@@ -178,30 +214,33 @@
 			);
 	};
 
+
 	/* button configs */
 	const gameArea = document.getElementById('gameArea'),
 		  playButton = document.getElementById('playButton'),
-		  weaponArea = document.getElementById('weaponArea');
+		  weaponArea = document.getElementById('weaponArea'),
+		  resetButton = document.getElementById('resetButton');
 
-	let resetButton = document.getElementById('resetButton');
-		resetButton.onclick = function () {
-			human.reset();
-			cpu.reset();
-			gameBoard.reset( document );
-			gameArea.classList.add( 'hide' );
-			weaponArea.classList.remove( 'hide' );
+	resetButton.onclick = function () {
+		human.reset();
+		cpu.reset();
+		gameBoard.reset( window );
+		gameArea.classList.add( 'hide' );
+		weaponArea.classList.remove( 'hide' );
 
-		}
+	}
 
 	playButton.onclick = function () {
 		weaponArea.classList.add( 'hide' );
 		gameArea.classList.remove( 'hide' );
-		gameBoard.init( document, human );
+
+		cpu.configWeapon( human );
+		gameBoard.init( window, human, cpu);
 		
 	}
 
 
-	weaponChooser.init( document, human );
+	weaponChooser.init( window, human );
 
 	
 
@@ -213,4 +252,4 @@
 	}
 
 
-})()
+})( window )
